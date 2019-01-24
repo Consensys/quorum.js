f74a3e282285e1ceacf643c99613ed0b1ffe6ca4
Felix Shnir <felix.shnir@gmail.com>
2019-01-24 16:26:49 +0000

    Adding example for entending web3

---
+++ 7nodes-test/extension.js
@@ -0,0 +1,14 @@
+const Web3 = require("web3");
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider("http://localhost:22000")
+);
+
+const quorumjs = require("../lib/index.js");
+
+// Call extend to add Quorum into web3 instance 
+quorumjs.extend(web3);
+
+
+// Example of calling Quorum specific API
+web3.quorum.raft.leader().then(function(o,e) { console.log(o, e) });
